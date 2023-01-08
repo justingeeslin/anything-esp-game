560252d38786bd2b0188dc2ad8afb1ff9dff96fd
Justin Geeslin <justin@geesl.in>
2023-01-07 22:00:39 -0600

    Moving the word counting function up to common

--- results.php
+++ results.php
@@ -5,32 +5,7 @@ header('Cache-Control: max-age=0');
 
 include('common.php');
 
-$dir = new DirectoryIterator(dirname(__FILE__));
-$users = array();
-
-$results = array();
-
-foreach ($dir as $fileinfo) {
-    if (isResultsFile($fileinfo)) {
-        
-        // Read the CSV
-        $csv = array_map( 'str_getcsv', file( $fileinfo->getFilename() ) );
-        // var_export($csv);
-
-        foreach ($csv as $entry) {
-            $image = $entry[0];
-            $words = $entry[1];
-
-            // Make this one big long string of words for now. We'll explode and count values later.
-            $results[$image] .= $words . ",";
-        }
-    }
-}
-
-// For each image count the words and replace that entry with an assoc array of word => count
-foreach($results as $image => $words) {
-    $results[$image] = array_count_values(explode(",", $words));
-}
+$results = getResults();
 
 // var_export($results);
 $out = fopen('php://output', 'w');
